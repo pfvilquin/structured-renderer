38f556851b630ad52835cd5ecf4b8370ddfe3d53
Manuela Caicedo <manuela.caicedo@symphony.com>
2018-01-22 12:49:02 -0500

    iframe error fix

--- src/javascript/controller.js
+++ src/javascript/controller.js
@@ -81,7 +81,7 @@ SYMPHONY.remote.hello().then(function(data) {
                 if( entityData.version === "1.0" ) {
                     template = `<messageML>
                     <hash tag="#tradeAlert"/>
-                                  <iframe src="https://so-extension-app.firebaseapp.com/app.html?email=`+email+`&alert=true" height="400" width="300"></iframe>
+                                  <iframe src="https://localhost:4000/app.html?email=`+email+`&amp;alert=true" height="400" width="300"></iframe>
                                </messageML>`
                 } 
                 return {
